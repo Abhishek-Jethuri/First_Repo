a9b136e71fdfb1df904b35565e1d6ae8f2de6632
Abhishek-Jethuri <abhishek.jethuri@gmail.com>
2023-08-19 15:34:12 +0530

    Implemented print table function
    It takes a vector of rows.

--- Utilities.cpp
+++ Utilities.cpp
@@ -1,11 +1,54 @@
-#pragma once
+// #pragma once
 
 #include <iostream>
 #include <vector>
+#include <iomanip>
+// using namespace std;
 
-template <typename T>
-void PrintTable(std::vector<std::vector<T>> data){
-    std:: cout << "TODO: Print table\n";
+void PrintTable(std::vector<std::vector<std::string>> data){
+    size_t numberOfRows = data.size();
+    size_t numberOfColumns = data[0].size();
+    std:: vector<size_t> columnWidths(numberOfColumns);
+    for (const auto& row : data){
+        for (size_t i = 0; i < numberOfColumns; i++){
+            size_t length = row[i].length();
+            if (length > columnWidths[i])
+                columnWidths[i] = length;
+        }
+    }
+
+    std:: cout << '+';
+    for (const auto width : columnWidths){
+        for (int j = 0; j < width; j++)
+            std:: cout << '-';
+        std:: cout << '+';
+    }
+    std:: cout << '\n';
+
+    for (const auto& row : data){
+        std:: cout << '|';
+        for (size_t i = 0; i < numberOfColumns; i++){
+            std:: cout << std:: setw(columnWidths[i]) << row[i] << '|';
+        }
+        std:: cout << '\n';
+
+        std:: cout << '+';
+        for (const auto width : columnWidths){
+            for (int j = 0; j < width; j++)
+                std:: cout << '-';
+            std:: cout << '+';
+        }
+        std:: cout << '\n';
+    }
+
 }
 
 // To add: Another Function
+int main (){
+    using namespace std;
+    vector<vector<string>> table = 
+    {{"Days/Time", "8-9", "9-10", "10-11", "11-12"},
+    {"Monday", "Web T", "DAA", "CAO", "MnM"}};
+
+    PrintTable(table);
+}
